004299329960e1f282a26863ec3df11a0facec02
Fabien Potencier <fabien.potencier@gmail.com>
2010-11-06 16:04:23 +0100

    fixed front controllers

--- web/app.php
+++ web/app.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__.'/../app/AppKernel.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $kernel = new AppKernel('prod', false);
-$kernel->handle()->send();
+$kernel->handle(new Request())->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -8,5 +8,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
 
 require_once __DIR__.'/../app/AppKernel.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $kernel = new AppKernel('dev', true);
-$kernel->handle()->send();
+$kernel->handle(new Request())->send();
