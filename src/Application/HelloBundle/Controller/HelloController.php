057f54ef500e731b26058288668eb09b9410caed
Fabien Potencier <fabien.potencier@gmail.com>
2010-11-23 18:54:21 +0100

    switched to the Twig templates by default

--- src/Application/HelloBundle/Controller/HelloController.php
+++ src/Application/HelloBundle/Controller/HelloController.php
@@ -8,9 +8,9 @@ class HelloController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('HelloBundle:Hello:index.php', array('name' => $name));
+        return $this->render('HelloBundle:Hello:index.twig', array('name' => $name));
 
-        // render a Twig template instead
-        // return $this->render('HelloBundle:Hello:index.twig', array('name' => $name));
+        // render a PHP template instead
+        // return $this->render('HelloBundle:Hello:index.php', array('name' => $name));
     }
 }
