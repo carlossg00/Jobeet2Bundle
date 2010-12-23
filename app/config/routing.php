65c20a51b2817e0dbad44b2208802a9edb4e97f9
Joseph Rouff <rouffj@gmail.com>
2010-12-23 10:16:31 +0100

    Rename addRoute() methods in routing files
    Rename $collection->addRoute to $collection->add in routing
    files (see 944d91c1 in symfony/symfony).

--- app/config/routing.php
+++ app/config/routing.php
@@ -4,7 +4,7 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
-$collection->addRoute('homepage', new Route('/', array(
+$collection->add('homepage', new Route('/', array(
     '_controller' => 'FrameworkBundle:Default:index',
 )));
 $collection->addCollection($loader->import("HelloBundle/Resources/config/routing.php"));
--- src/Application/HelloBundle/Resources/config/routing.php
+++ src/Application/HelloBundle/Resources/config/routing.php
@@ -4,7 +4,7 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
-$collection->addRoute('hello', new Route('/hello/:name', array(
+$collection->add('hello', new Route('/hello/:name', array(
     '_controller' => 'HelloBundle:Hello:index',
 )));
 
