f9f4b7d3b5647167829bd1b1860f59b7c40f049f
Carlossg <carlossg00@hotmail.com>
2011-02-28 11:05:08 +0100

    Bundle Semantic Configuration

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\Jobeet2Bundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * This class contains the configuration information for the bundle
+ *
+ * This information is solely responsible for how the different configuration
+ * sections are normalized, and merged.
+ *
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class Configuration
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return \Symfony\Component\Config\Definition\NodeInterface
+     */
+    public function getConfigTree()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('jobeet2', 'array');
+
+        $rootNode
+            ->scalarNode('max_jobs_on_homepage')->defaultValue('10')->end()
+            ->scalarNode('active_days')->defaultValue('30')->end()
+            ->scalarNode('max_jobs_on_category')->defaultValue('20')->end()           
+            ->end();          
+            
+        return $treeBuilder->buildTree();
+    }
+}
--- DependencyInjection/Jobeet2Extension.php
+++ DependencyInjection/Jobeet2Extension.php
@@ -6,21 +6,47 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-
+use Symfony\Component\Config\Definition\Processor;
+   
 
 class Jobeet2Extension extends Extension
 {
-    public function load(array $config, ContainerBuilder $container)
+	 /**
+     * Loads the Jobeet2 configuration.
+     *
+     * Usage example:
+     *
+     *      <jobeet2:config>
+     *        <jobeet2:max_jobs_on_homepage>10</jobeet2:max_jobs_on_homepage>
+     *        <jobeet2:active_days>10</jobeet2:active_days>
+     *      </jobeet2:config>
+     *
+     * @param array            $config    An array of configuration settings
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+	
+    public function load(array $configs, ContainerBuilder $container)
     {
+    	    	
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        
+                
         $loader->load('config.xml');
-    
-        foreach($config as $key => $value) {        
-            $container->setParameter('jobeet2.'.$key, $value);
+        
+        $configuration = new Configuration();
+        $processor = new Processor();
+        $config = $processor->process($configuration->getConfigTree(), $configs);
+
+        foreach ($config as $k => $v)
+        {
+        	$container->setParameter('jobeet2.'.$k, $v);
         }
         
-        $loader->load('controller.xml');       
+        
+        $loader->load('model.xml');
+        $loader->load('orm.xml');        
+        $loader->load('controller.xml');
+        $loader->load('form.xml');  
+         
     }
 
     public function getXsdValidationBasePath()
@@ -32,10 +58,5 @@ class Jobeet2Extension extends Extension
     public function getNamespace()
     {
         return 'http://www.symfony-project.org/schema/dic/symfony';
-    }
-
-    public function getAlias()
-    {
-        return 'jobeet2';
-    }
+    }   
 }
